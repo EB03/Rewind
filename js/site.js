9fc755ab2affec34308df77a6825488cb5efe884
EB03 <35344980+EB03@users.noreply.github.com>
2021-08-22 13:59:49 -0400

    Finished Logic

--- js/site.js
+++ js/site.js
@@ -1,4 +1,27 @@
-// Call Hello World
-function HelloWorld() {
-    alert("Hello World");
+
+function getText() {
+    document.getElementById("result").classList.add("invisible");
+
+    let startText = document.getElementById("reverseText").value;
+    let rText = reverseText(startText);
+    displayText(rText);
+}
+
+function reverseText(startText){
+    let rText = [];
+
+    //reverse a string using a for loop
+    for (let i = startText.length - 1; i >= 0; i--) {
+        rText += startText[i];
+    }
+
+    return rText;
+}
+
+function displayText(rText) {
+    
+    // write to the page
+    document.getElementById('message').innerHTML = `Your string reversed is: ${rText}`;
+    // show the alert box
+    document.getElementById('result').classList.remove("invisible");
 }
